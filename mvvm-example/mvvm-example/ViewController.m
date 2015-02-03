b6fb3cba0443c68f6a311fb1a74e1524446e1b3b
tjohnson640 <tyler@livefront.com>
2015-02-02 20:03:13 -0600

    Initial project commit.

---
+++ mvvm-example/mvvm-example/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mvvm-example
+//
+//  Created by Tyler Johnson on 2/2/15.
+//  Copyright (c) 2015 livefront. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
